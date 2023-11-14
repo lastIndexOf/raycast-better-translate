210efaae579e3db18d5f692a5db4e4a05db66d0b
cold_sharp <kgaikj2cu@icloud.com>
2023-11-14 19:58:56 +0800

    chore: learn some objc basic & binding knowledge

--- ocr/src/main.rs
+++ ocr/src/main.rs
@@ -2,49 +2,27 @@ use std::ffi::c_void;
 use std::time::Instant;
 
 use cocoa::appkit::{
-    NSApp, NSApplication, NSApplicationActivationPolicyRegular, NSBackingStoreBuffered, NSImage,
-    NSImageView, NSView, NSWindow, NSWindowStyleMask,
+    NSApp, NSApplication, NSApplicationActivationPolicyRegular, NSBackingStoreBuffered, NSColor,
+    NSEvent, NSImage, NSImageView, NSRectFill, NSView, NSWindow, NSWindowStyleMask,
 };
 use cocoa::base::{id, nil, NO};
 use cocoa::foundation::{NSAutoreleasePool, NSData, NSPoint, NSRect, NSSize, NSUInteger};
+use core_graphics::context::CGContextRef;
 use core_graphics::display::{CGDisplay, CGDisplayBounds};
+use core_graphics::geometry::{CGPoint, CGRect, CGSize};
+use core_graphics::sys::CGContext;
 use objc::declare::ClassDecl;
+use objc::rc::StrongPtr;
 use objc::runtime::{Class, Object, Sel};
-use objc::{msg_send, sel, sel_impl};
+use objc::{class, msg_send, sel, sel_impl};
 use screenshots::image::codecs::png::PngEncoder;
 use screenshots::image::{ColorType, ImageEncoder};
 use screenshots::Screen;
 
-extern "C" fn mouse_down(_this: &Object, _sel: Sel, event: id) {
-    println!("Mouse button pressed.");
-}
-
-extern "C" fn mouse_dragged(_this: &Object, _sel: Sel, event: id) {
-    println!("Mouse dragged.");
-}
-
-extern "C" fn mouse_up(_this: &Object, _sel: Sel, event: id) {
-    println!("Mouse button released.");
-}
-
-unsafe fn create_custom_view_class(view_id: u32) -> *mut Object {
-    let cls_name = format!("Display{view_id}EventHandlerClass");
-    let superclass = Class::get("NSView").unwrap();
-    let mut decl = ClassDecl::new(&cls_name, superclass).unwrap();
-
-    decl.add_method(
-        sel!(mouseDown:),
-        mouse_down as extern "C" fn(&Object, Sel, id),
-    );
-    decl.add_method(
-        sel!(mouseDragged:),
-        mouse_dragged as extern "C" fn(&Object, Sel, id),
-    );
-    decl.add_method(sel!(mouseUp:), mouse_up as extern "C" fn(&Object, Sel, id));
-
-    let my_view_class = decl.register();
-
-    msg_send![my_view_class, new]
+struct State {
+    start_point: Option<(f64, f64)>,
+    end_point: Option<(f64, f64)>,
+    pressed: bool,
 }
 
 fn main() -> anyhow::Result<()> {
@@ -66,6 +44,12 @@ fn main() -> anyhow::Result<()> {
         let mut offset = (0.0, None);
         for display in displays {
             if let Some(screen) = screens.iter().find(|ele| ele.display_info.id == display) {
+                let state = State {
+                    start_point: None,
+                    end_point: None,
+                    pressed: false,
+                };
+
                 let screen_rect = CGDisplayBounds(display);
                 let screen_size = NSSize::new(
                     screen_rect.size.width as f64,
@@ -86,7 +70,52 @@ fn main() -> anyhow::Result<()> {
                     )
                     .autorelease();
 
-                window.setContentView_(create_custom_view_class(display));
+                extern "C" fn mouse_down(_this: &Object, _sel: Sel, _event: id) {
+                    unsafe {
+                        let mouse_location = NSEvent::mouseLocation(nil);
+                        println!(
+                            "Mouse button pressed at x: {}, y: {}",
+                            mouse_location.x, mouse_location.y
+                        );
+                    }
+                }
+                extern "C" fn mouse_up(_this: &Object, _sel: Sel, _event: id) {
+                    unsafe {
+                        let mouse_location = NSEvent::mouseLocation(nil);
+                        println!(
+                            "Mouse button pressed at x: {}, y: {}",
+                            mouse_location.x, mouse_location.y
+                        );
+                    }
+                }
+
+                extern "C" fn draw_rect(this: &Object, _sel: Sel, _rect: NSRect) {
+                    unsafe {
+                        let blue: id = msg_send![class!(NSColor), colorWithCalibratedRed:0.0 green:0.0 blue:1.0 alpha:0.1];
+                        let _: () = msg_send![blue, setFill];
+                        let rect = msg_send![NSMakeRect, 0.0, 0., 300., 300.];
+                        // NSRectFill(NSRect::new(
+                        //     NSPoint::new(0., 0.),
+                        //     NSSize::new(300.0, 300.0),
+                        // ));
+                        println!("({}, {})", _rect.size.width, _rect.size.height);
+                        NSRectFill(_rect);
+                    }
+                }
+                let cls_name = format!("Display{display}EventHandlerView");
+                let superclass = Class::get("NSView").unwrap();
+                let mut decl = ClassDecl::new(&cls_name, superclass).unwrap();
+                decl.add_method(
+                    sel!(mouseDown:),
+                    mouse_down as extern "C" fn(&Object, Sel, id),
+                );
+                decl.add_method(sel!(mouseUp:), mouse_up as extern "C" fn(&Object, Sel, id));
+                decl.add_method(
+                    sel!(drawRect:),
+                    draw_rect as extern "C" fn(&Object, Sel, NSRect),
+                );
+                let my_view_class = decl.register();
+                let custom_view: id = msg_send![my_view_class, new];
 
                 let now = Instant::now();
                 // 截屏
@@ -122,12 +151,13 @@ fn main() -> anyhow::Result<()> {
                     cocoa::appkit::NSViewWidthSizable | cocoa::appkit::NSViewHeightSizable,
                 );
 
+                image_view.addSubview_(custom_view);
                 // 将 NSImageView 添加到窗口的内容视图
                 window.contentView().addSubview_(image_view);
-
                 // 设置窗口层级，使其覆盖Dock和菜单栏
                 window.setLevel_(cocoa::appkit::NSMainMenuWindowLevel as i64 + 2);
                 window.setOpaque_(NO);
+                // 直接让窗口透明，截图动态图片
                 // window.setBackgroundColor_(cocoa::appkit::NSColor::clearColor(nil));
                 window.makeKeyAndOrderFront_(nil);
 
