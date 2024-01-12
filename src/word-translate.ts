76a2b1504d9b3946af137b76ff2911511cc7ba6c
cold_sharp <kgaikj2cu@icloud.com>
2024-01-12 16:30:40 +0800

    chore: update getSelectedWord to racyast official api

--- src/word-translate.ts
+++ src/word-translate.ts
@@ -1,7 +1,6 @@
-import { open } from "@raycast/api";
-import { readSelectionWord } from "./helper";
+import { getSelectedText, open } from "@raycast/api";
 
 export default async function main() {
-  const word = await readSelectionWord();
+  const word = await getSelectedText();
   await open(`raycast://extensions/raycast/translator/translate?fallbackText=${word}`);
 }
